b5730556f769442db933214475d0f2257649e245
vaibhavramchandani <vaibhav_ramchandani@persistent.com>
2021-07-22 12:30:56 +0530

    from cmd

--- src/main/java/com/devskiller/calculator/Calculator.java
+++ src/main/java/com/devskiller/calculator/Calculator.java
@@ -3,7 +3,7 @@ package com.devskiller.calculator;
 public class Calculator {
 
 	public int add(int a, int b) {
-		return a +b;
+		return a - b;
 	}
 
 	public int subtract(int a, int b) {
@@ -11,15 +11,14 @@ public class Calculator {
 	}
 
 	public int multiply(int a, int b) {
-		return a*b;
+		return a * b;
 	}
 
 	public int divide(int a, int b) {
-		if(b == 0)
-		{
-		   throw new IllegalArgumentException();
+		if (b == 0) {
+			throw new IllegalArgumentException();
 		}
-		return a/b;
+		return a / b;
 	}
 
 }
